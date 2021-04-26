8e54b94fc406a77e2b5124b2c8e64e84533b29d4
Dima Sivov <dsivov@gmail.com>
2021-04-26 06:15:33 +0000

    bug with vector counting is fixed

--- paragraphvec/export_vectors.py
+++ paragraphvec/export_vectors.py
@@ -9,6 +9,8 @@ from paragraphvec.data import load_dataset
 from paragraphvec.models import DM, DBOW
 from paragraphvec.utils import DATA_DIR, MODELS_DIR
 
+from simpleneighbors import SimpleNeighbors
+from arango import ArangoClient
 
 
 def start(data_file_name, model_file_name):
@@ -32,8 +34,12 @@ def start(data_file_name, model_file_name):
         vec_dim,
         num_docs=len(dataset),
         num_words=len(dataset.fields['text'].vocab) - 1)
-    print(model)
-    _write_to_file(data_file_name, model_file_name, model, vec_dim)
+    
+    #_write_to_file(data_file_name, model_file_name, model, vec_dim)
+    db = connect_db("nebula_dev")
+    index = _write_to_annoy(db, model, vec_dim)
+    input_loop(index)
+
 
 
 def _load_model(model_file_name, vec_dim, num_docs, num_words):
@@ -84,6 +90,41 @@ def _write_to_file(data_file_name, model_file_name, model, vec_dim):
         writer = csv.writer(f)
         writer.writerows(result_lines)
 
+def _write_to_annoy(db, model, vec_dim):
+    query = 'FOR doc IN Stories RETURN doc'  
+    stories = []
+    embedding_dimensions = vec_dim
+    num_index_trees = 512
+    single_index = SimpleNeighbors(embedding_dimensions) 
+    cursor = db.aql.execute(
+            query
+        )
+    for i, data in enumerate(cursor):
+        key = data['movie_id']
+        vector = model.get_paragraph_vector(i - 1)
+        single_index.add_one(key, vector)
+    single_index.build(n=num_index_trees) 
+    return(single_index)
+
+def input_loop(index):
+    while (True):
+            _key = input("Enter movie id: ")
+            #Key -> movie id (Movies/1111111), Top -> number of similar movies
+            sims = index.neighbors(_key, n=10)
+            if sims:
+                print("----------------------") 
+                print("Top 10 Sims for Movie: " + _key)
+                for sim in sims:
+                    print(sim)
+                print("----------------------")  
+            else:
+                print("Please rebuild index")
+
+def connect_db(dbname):
+    #client = ArangoClient(hosts='http://ec2-18-219-43-150.us-east-2.compute.amazonaws.com:8529')
+    client = ArangoClient(hosts='http://18.159.140.240:8529')
+    db = client.db(dbname, username='nebula', password='nebula')
+    return (db)
 
 if __name__ == '__main__':
     fire.Fire()
